ec0df4e573d30fbf24109151636ff228e323ceed
Stein Heselmans <teh@melexis.com>
2017-06-01 16:27:27 +0200

    More test cases

--- test/jira_juggler_test.py
+++ test/jira_juggler_test.py
@@ -96,13 +96,12 @@ class TestJiraJuggler(unittest.TestCase):
         juggler = dut.JiraJuggler(self.URL, self.USER, self.PASSWD, self.QUERY)
         self.assertEqual(self.QUERY, juggler.query)
 
-        data = self.JIRA_JSON_ISSUE_TEMPLATE.format(key=self.KEY1,
-                                                    summary=self.SUMMARY1,
-                                                    assignee=self.ASSIGNEE1,
-                                                    estimate=self.ESTIMATE1,
-                                                    depends=self.DEPENDS1)
-        issue1 = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        jira_mock_object.search_issues.side_effect = [[issue1], []]
+        jira_mock_object.search_issues.side_effect = [[self._mock_jira_issue(self.KEY1,
+                                                                             self.SUMMARY1,
+                                                                             self.ASSIGNEE1,
+                                                                             self.ESTIMATE1,
+                                                                             self.DEPENDS1)
+                                                       ], []]
         issues = juggler.juggle()
         jira_mock_object.search_issues.assert_has_calls([call(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=0),
                                                          call(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=1)])
@@ -114,6 +113,28 @@ class TestJiraJuggler(unittest.TestCase):
         self.assertEqual([], issues[0].properties['depends'].get_value())
 
     @patch('jira_juggler.jira_juggler.JIRA', autospec=True)
+    def test_single_task_minimal(self, jira_mock):
+        '''Test for minimal happy flow: single task with minimal content is returned by Jira'''
+        jira_mock_object = MagicMock(spec=JIRA)
+        jira_mock.return_value = jira_mock_object
+        juggler = dut.JiraJuggler(self.URL, self.USER, self.PASSWD, self.QUERY)
+        self.assertEqual(self.QUERY, juggler.query)
+
+        jira_mock_object.search_issues.side_effect = [[self._mock_jira_issue(self.KEY1,
+                                                                             self.SUMMARY1,
+                                                                             None,
+                                                                             self.ESTIMATE1, #TODO: None here
+                                                                             None),
+                                                       ], []]
+        issues = juggler.juggle()
+        jira_mock_object.search_issues.assert_has_calls([call(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=0),
+                                                         call(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=1)])
+        self.assertEqual(1, len(issues))
+        self.assertEqual(self.KEY1, issues[0].key)
+        self.assertEqual(self.SUMMARY1, issues[0].summary)
+
+
+    @patch('jira_juggler.jira_juggler.JIRA', autospec=True)
     def test_task_depends(self, jira_mock):
         '''Test for dual happy flow: one task depends on the other'''
         jira_mock_object = MagicMock(spec=JIRA)
@@ -121,19 +142,17 @@ class TestJiraJuggler(unittest.TestCase):
         juggler = dut.JiraJuggler(self.URL, self.USER, self.PASSWD, self.QUERY)
         self.assertEqual(self.QUERY, juggler.query)
 
-        data = self.JIRA_JSON_ISSUE_TEMPLATE.format(key=self.KEY1,
-                                                    summary=self.SUMMARY1,
-                                                    assignee=self.ASSIGNEE1,
-                                                    estimate=self.ESTIMATE1,
-                                                    depends=self.DEPENDS1)
-        issue1 = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        data = self.JIRA_JSON_ISSUE_TEMPLATE.format(key=self.KEY2,
-                                                    summary=self.SUMMARY2,
-                                                    assignee=self.ASSIGNEE2,
-                                                    estimate=self.ESTIMATE2,
-                                                    depends=self.DEPENDS2)
-        issue2 = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        jira_mock_object.search_issues.side_effect = [[issue1, issue2], []]
+        jira_mock_object.search_issues.side_effect = [[self._mock_jira_issue(self.KEY1,
+                                                                             self.SUMMARY1,
+                                                                             self.ASSIGNEE1,
+                                                                             self.ESTIMATE1,
+                                                                             self.DEPENDS1),
+                                                       self._mock_jira_issue(self.KEY2,
+                                                                             self.SUMMARY2,
+                                                                             self.ASSIGNEE2,
+                                                                             self.ESTIMATE2,
+                                                                             self.DEPENDS2),
+                                                       ], []]
         issues = juggler.juggle()
         jira_mock_object.search_issues.assert_has_calls([call(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=0),
                                                          call(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=2)])
@@ -148,6 +167,27 @@ class TestJiraJuggler(unittest.TestCase):
         self.assertEqual(self.ASSIGNEE2, issues[1].properties['allocate'].get_value())
         self.assertEqual(self.ESTIMATE2/self.SECS_PER_DAY, issues[1].properties['effort'].get_value())
         self.assertEqual(self.DEPENDS2, issues[1].properties['depends'].get_value()[0])
+
+    def _mock_jira_issue(self, key, summary, assignee=None, estimate=None, depends=None):
+        '''
+        Helper function to create a mocked Jira issue
+
+        Args:
+            key (str): Key of the mocked Jira issue
+            summary (str): Summary of the mocked Jira issue
+            assignee (str): Name of the assignee of the mocked Jira issue
+            estimate (float): Number of estimated seconds of the mocked Jira issue
+            depends (float): Key of the issue on which the mocked Jira issue depends (blocked by relation)
+
+        Returns:
+            object: Mocked Jira Issue object
+        '''
+        data = self.JIRA_JSON_ISSUE_TEMPLATE.format(key=key,
+                                                    summary=summary,
+                                                    assignee=assignee,
+                                                    estimate=estimate,
+                                                    depends=depends)
+        return json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
 if __name__ == '__main__':
     unittest.main()
