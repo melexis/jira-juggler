292b5f0ef3e1789c8a8cfd0970201e4b7db7a432
Stein Heselmans <teh@melexis.com>
2017-06-01 14:23:35 +0200

    Compacter test case

--- test/jira_juggler_test.py
+++ test/jira_juggler_test.py
@@ -25,10 +25,15 @@ except ImportError as err:
     pip.main(['install', 'jira'])
     from jira import JIRA, JIRAError
 
-from jira_juggler import jira_juggler
+from jira_juggler import jira_juggler as dut
 
 class TestJiraJuggler(unittest.TestCase):
     '''Testing JiraJuggler interface'''
+
+    URL = 'http://my-non-existing-jira.melexis.com'
+    USER = 'justme'
+    PASSWD = 'myuselesspassword'
+    QUERY = 'some random query'
 
     def SetUp(self):
         '''SetUp is run before each test to provide clean working environment'''
@@ -36,18 +41,14 @@ class TestJiraJuggler(unittest.TestCase):
     @patch('jira_juggler.jira_juggler.JIRA', autospec=True)
     def test_empty_query_result(self, jira_mock):
         '''Test for Jira not returning any task on the given query'''
+        jira_mock_object = MagicMock(spec=JIRA)
+        jira_mock.return_value = jira_mock_object
+        juggler = dut.JiraJuggler(self.URL, self.USER, self.PASSWD, self.QUERY)
+        self.assertEqual(self.QUERY, juggler.query)
 
-        url = 'http://my-non-existing-jira.melexis.com'
-        user = 'justme'
-        passwd = 'myuselesspassword'
-        q = 'some random query'
-        jira_mock.return_value = MagicMock(spec=JIRA)
-        juggler = jira_juggler.JiraJuggler(url, user, passwd, q)
-        self.assertEqual(q, juggler.query)
-
-        jira_mock.return_value.search_issues.return_value = []
+        jira_mock_object.search_issues.return_value = []
         juggler.juggle()
-        jira_mock.return_value.search_issues.assert_called_once_with(q, maxResults=jira_juggler.JIRA_PAGE_SIZE, startAt=0)
+        jira_mock_object.search_issues.assert_called_once_with(self.QUERY, maxResults=dut.JIRA_PAGE_SIZE, startAt=0)
 
 if __name__ == '__main__':
     unittest.main()
