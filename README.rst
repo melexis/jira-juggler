f7a351126ee3bff28aa6ee37290e565d7b18313d
Stein Heselmans <teh@melexis.com>
2017-06-19 15:41:37 +0200

    Folder structure taken over from warnings-plugin

---
+++ README.rst
@@ -0,0 +1,10 @@
+.. image:: https://img.shields.io/hexpm/l/plug.svg
+    :target: http://www.apache.org/licenses/LICENSE-2.0
+
+=============================
+JIRA to TaskJuggler convertor
+=============================
+
+Tool for converting a set of JIRA tasks to TaskJuggler (TJ3) syntax.
+
+.. todo:: More details soon
---
+++ setup.py
@@ -0,0 +1,51 @@
+import io
+from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import find_packages, setup
+
+PROJECT_URL = 'https://github.com/melexis/jira-juggler'
+VERSION = '1.0.0'
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='mlx.jira_juggler',
+    version=VERSION,
+    url=PROJECT_URL,
+    download_url=PROJECT_URL + '/tarball/' + VERSION,
+    author='Stein Heselmans',
+    author_email='teh@melexis.com',
+    description='A python script for extracting data from Jira, and converting to task-juggler (tj3) output',
+    long_description=open("README.rst").read(),
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    install_requires=['jira'],
+    namespace_packages=['mlx'],
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache2 License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = ['Jira', 'taskjuggler', 'gantt'],
+)
