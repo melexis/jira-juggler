ee44a9bb6f54254bce8110db69e454fde2bd8191
jce <jce@melexis.com>
2022-05-11 16:08:10 +0200

    Support authentication with API token and using .env file

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     package_dir={'': 'src'},
     entry_points={'console_scripts': ['jira-juggler = mlx.jira_juggler:entrypoint']},
     include_package_data=True,
-    install_requires=['jira', 'python-dateutil>=2.8.0,<3.*', 'natsort>=7.1.0,<8.*'],
+    install_requires=['jira', 'python-dateutil>=2.8.0,<3.*', 'natsort>=7.1.0,<8.*', 'python-decouple'],
     setup_requires=['setuptools_scm'],
     namespace_packages=['mlx'],
     classifiers=[
