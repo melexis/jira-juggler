cbd882deb6b389d842926ea43fbc9fc740e7599a
jce <jce@melexis.com>
2022-07-07 10:41:57 +0200

    Limit version range of setuptools-scm to keep supporting Python 3.6

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     entry_points={'console_scripts': ['jira-juggler = mlx.jira_juggler:entrypoint']},
     include_package_data=True,
     install_requires=['jira', 'python-dateutil>=2.8.0,<3.*', 'natsort>=7.1.0,<8.*', 'python-decouple'],
-    setup_requires=['setuptools_scm'],
+    setup_requires=['setuptools-scm>=6.0.0,<7.*'],
     namespace_packages=['mlx'],
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
