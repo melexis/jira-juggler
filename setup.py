29ad9b4f39b8a3908ae74a977dae1ad5de7da43b
jce <jce@melexis.com>
2023-10-13 10:20:25 +0200

    Remove upper version limit for dependencies

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
     package_dir={'': 'src'},
     entry_points={'console_scripts': ['jira-juggler = mlx.jira_juggler:entrypoint']},
     include_package_data=True,
-    install_requires=['jira>=3.1.1', 'python-dateutil>=2.8.0,<3.0', 'natsort>=7.1.0,<8.0', 'python-decouple'],
-    setup_requires=['setuptools-scm>=6.0.0,<8.0'],
+    install_requires=['jira>=3.1.1', 'python-dateutil>=2.8.0', 'natsort>=7.1.0', 'python-decouple'],
+    setup_requires=['setuptools-scm>=6.0.0'],
     python_requires='>=3.7',
     namespace_packages=['mlx'],
     classifiers=[
